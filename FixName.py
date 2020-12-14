5ca9291f508f18446e83d12b400a6a7176273550
Srecko Micic <srecko.micic@gmail.com>
2020-12-14 16:50:45 +0100

    First Script

---
+++ FixName.py
@@ -0,0 +1,21 @@
+import modo
+
+scene = modo.Scene()
+
+for itm in scene.selected:
+
+	name = itm.name
+	print ('Old name: %s') % name
+	itm.name = itm.name.replace(" ", "_")
+	itm.name = itm.name.replace(".", "_")
+	itm.name = itm.name.replace(":", "")
+	itm.name = itm.name.replace('"', "")
+	itm.name = itm.name.replace('/', "")
+	itm.name = itm.name.replace('[', "")
+	itm.name = itm.name.replace(']', "")
+	itm.name = itm.name.replace('-', "")
+	itm.name = itm.name.replace("'", "")
+	itm.name = itm.name.replace("@", "")
+
+	print ('New name: %s') % itm.name
+	
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MODO-Scripts
+
+
+Here is collection of MODO Scripts I made to speedup my workflow with Foundry MODO!
+
